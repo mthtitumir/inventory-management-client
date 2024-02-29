34b3bb3efe06f09f5f4f7c2fe32ed8809458d6c6
mthtitumir <titumirgccc@gmail.com>
2024-02-29 22:11:53 +0600

    search design modified

--- src/components/ui/SearchBox.tsx
+++ src/components/ui/SearchBox.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { SearchOutlined } from '@ant-design/icons';
-import { Input, Select, Space } from 'antd';
+// import { SearchOutlined } from '@ant-design/icons';
+import { Flex, Input, Select, Space } from 'antd';
+import { IoIosArrowDown } from "react-icons/io";
+import { FaSearchengin } from "react-icons/fa";
 
 const SearchBox: React.FC = () => {
     const [searchIn, setSearchIn] = useState("customers")
@@ -26,14 +28,18 @@ const SearchBox: React.FC = () => {
             label: 'Discount',
         },
     ];
-    const handleChangeSelect = (value: string) =>{
+    const handleChangeSelect = (value: string) => {
         console.log(value);
         setSearchIn(value);
-        
+
     }
+    const searchIcons = (<Flex align='center' gap={2} style={{ width: "" }}>
+        <FaSearchengin size={16} color='maroon' style={{ marginLeft: "5px" }} />
+        <IoIosArrowDown size={16} color="maroon" style={{ marginRight: "-8px" }} />
+    </Flex>)
     return (
-        <Space.Compact  style={{backgroundColor: ""}}>
-            <Select onChange={handleChangeSelect} dropdownStyle={{ width: '300px' }} placeholder={<SearchOutlined />} value={<SearchOutlined />} options={options} />
+        <Space.Compact style={{ backgroundColor: "" }}>
+            <Select value={""} onChange={handleChangeSelect} dropdownStyle={{ width: '300px' }} suffixIcon={searchIcons} options={options} />
             <Input size='middle' style={{ width: "300px" }} placeholder={`Search is ${searchIn} ( / )`} />
         </Space.Compact>
     )
