a5b8e7d01b12a39d1d3f4e339dc4d63e2b681e26
mthtitumir <titumirgccc@gmail.com>
2024-03-03 20:05:30 +0600

    image upload function reused

--- src/components/form/AddUpdateFlower.tsx
+++ src/components/form/AddUpdateFlower.tsx
@@ -2,12 +2,16 @@
 import { useEffect, useState } from 'react';
 import {
     Button,
+    Col,
     Flex,
     Form,
     Input,
+    Row,
     Select,
     Upload,
+    UploadProps
 } from 'antd';
+// import { UploadOutlined } from "@ant-design/icons";
 import { flowerCategoryType, flowerFragrances, flowerSizes } from '../../constants/flower.constant';
 import toast from 'react-hot-toast';
 import { useAddFlowerMutation, useGetSingleFlowerQuery, useUpdateFlowerMutation } from '../../redux/features/flower/flowerApi';
@@ -17,45 +21,33 @@ import { FlowerDefaultValuesProps } from '../../types';
 import Spinner from '../ui/Spinner';
 import { useParams } from 'react-router-dom';
 import AddHeader from '../ui/AddHeader';
+import { uploadImage } from '../../utils/uploadImage';
 
 const AddUpdateFlower = ({ type }: { type: "add" | "update" | "variant" }) => {
     const { itemId } = useParams();
-    const { data, isLoading } = useGetSingleFlowerQuery(itemId);
+    const { data, isLoading } = useGetSingleFlowerQuery(itemId, { skip: !itemId });
     const [addFlower] = useAddFlowerMutation();
     const [updateFlower] = useUpdateFlowerMutation();
     const [imageUrl, setImageUrl] = useState('');
     const [isRequired, setIsRequired] = useState(true);
-    const [defaultValues, setDefaultValues] = useState<FlowerDefaultValuesProps>({ style: "fff", bloomDate: "2024-01-28", arrangement: "hello" });
+    const [defaultValues, setDefaultValues] = useState<FlowerDefaultValuesProps>({ style: "Compact", bloomDate: "2024-01-28", arrangement: "Loose and Wild" });
     const seller: TUser | null = useAppSelector(useCurrentUser);
-    console.log({ itemId });
+    console.log({ imageUrl });
 
-
-    const uploadImage = async (event: any) => {
-        // console.log(event.target.files[0]);
-        if (!event.target.files[0]) return;
-        const formData = new FormData();
-        formData.append("image", event.target.files[0]);
-        const toastId = toast.loading("Image uploading...");
-        try {
-            const res = await fetch(
-                `https://api.imgbb.com/1/upload?key=8232356251c40167197e4cb1208b3e70`,
-                {
-                    method: "POST",
-                    body: formData,
-                }
-            );
-            if (!res.ok) throw new Error("Failed to upload image");
-
-            const data = await res.json();
-            // console.log(data);
-            setImageUrl(data?.data?.display_url)
-            toast.dismiss(toastId);
-            toast.success("Image uploaded successfully!");
-        } catch (error) {
-            toast.error("Image not uploaded!");
-            toast.dismiss(toastId);
-        }
+    const props: UploadProps = {
+        name: 'file',
+        listType: "picture-card",
+        beforeUpload: (_file: any) => {
+            // Prevent automatic upload
+            return false;
+        },
+        async onChange(info) {
+            const url = await uploadImage(info.file);
+            setImageUrl(url);
+        },
+        maxCount: 1
     };
+
     // const { name, price, quantity, color, bloomDate, style, arrangement, type, size, fragrance, image } = data?.data;
     const onfinish = (values: any) => {
         if (!itemId && type === "add") {
@@ -86,89 +78,102 @@ const AddUpdateFlower = ({ type }: { type: "add" | "update" | "variant" }) => {
     }
     useEffect(() => {
         if (itemId && data) {
-            console.log(itemId);
-
+            // console.log(itemId);
             setDefaultValues(data?.data);
             setImageUrl(data?.data?.image);
             setIsRequired(false);
         }
     }, [itemId, data, type]);
     // console.log(defaultValues);
-    console.log({ isLoading, isRequired });
+    // console.log({ isLoading, isRequired });
     const typeTextMap = {
         "update": "Update Item",
         "variant": "Make New Variant",
         "add": "New Item"
     };
+
     return (
         isLoading || isRequired && type === ("update" || "variant") ? <Spinner /> :
             <>
                 {(type === "update" || type === "variant") && (!itemId || !data || isRequired) ? <Spinner /> :
-                    <Flex vertical >
+                    <div>
                         <AddHeader text={typeTextMap[type]} />
                         {/* <h2 style={{ textAlign: "center", marginBottom: "10px" }}>{!id && !data && "Add New Flower"}{type === "update" && "Update Flower"}{type === "variant" && "Make Variant"}</h2> */}
-                        <Flex justify='center' align='center'>
-                            <Form
-                                layout="horizontal"
-                                initialValues={defaultValues}
-                                onFinish={onfinish}
-                                style={{ width: "100%", padding: "20px" }}
-                            >
-                                <Form.Item name={"name"} label="Flower Name" rules={[{ required: isRequired, message: 'Please input your flower name!' }]}>
-                                    <Input defaultValue={defaultValues?.name} placeholder='Enter Flower Name' />
-                                </Form.Item>
-                                <Form.Item name={"price"} label="Price per piece" rules={[{ required: isRequired, message: 'Please input your flower price!' }]}>
-                                    <Input defaultValue={defaultValues?.price} type='number' placeholder='Enter Price' />
-                                </Form.Item>
-                                <Form.Item label="Upload picture" rules={[{ required: isRequired, message: 'Please upload your flower photo!' }]} >
-                                    <Upload>
-                                        <Input type='file' onChange={uploadImage} placeholder='Picture' />
-                                    </Upload>
-                                </Form.Item>
-                                <Form.Item name={"quantity"} label="Available quantity" rules={[{ required: isRequired, message: 'Please input your flower quantity!' }]} >
-                                    <Input defaultValue={defaultValues?.quantity} type='number' placeholder='Enter quantity' />
-                                </Form.Item>
-                                <Form.Item name={"color"} label="Color of the Flower" rules={[{ required: isRequired, message: 'Please input your flower color!' }]}>
-                                    <Input defaultValue={defaultValues?.color} placeholder='Enter color' />
-                                </Form.Item>
-                                <Form.Item name={"bloomDate"} label="Approximate bloom date">
-                                    <Input defaultValue={defaultValues?.bloomDate} type={'date' || "text"} placeholder='Enter date' />
-                                </Form.Item>
-                                <Form.Item name={"style"} label="Flower Style">
-                                    <Input defaultValue={defaultValues?.style} placeholder='Style' />
-                                </Form.Item>
-                                <Form.Item name={"arrangement"} label="Flower Arrangement">
-                                    <Input defaultValue={defaultValues?.arrangement} placeholder='Arrangement' />
-                                </Form.Item>
-                                <Form.Item name={"type"} label="Select flower category" rules={[{ required: isRequired, message: 'Please select your flower type!' }]} >
-                                    <Select defaultValue={defaultValues?.type} placeholder="Select Flower Type">
-                                        {
-                                            flowerCategoryType?.map((type) => <Select.Option value={type}>{type.toUpperCase()}</Select.Option>)
-                                        }
-                                    </Select>
-                                </Form.Item>
-                                <Form.Item name={"size"} label="Select Available Size" rules={[{ required: isRequired, message: 'Please input available flower sizes!' }]} >
-                                    <Select defaultValue={defaultValues?.size} placeholder="Select Size">
-                                        {
-                                            flowerSizes?.map((size) => <Select.Option value={size}>{size.toUpperCase()}</Select.Option>)
-                                        }
-                                    </Select>
-                                </Form.Item>
-                                <Form.Item name={"fragrance"} label="Fragrance Profile" rules={[{ required: isRequired, message: 'Please input your flower fragrance!' }]}>
-                                    <Select defaultValue={defaultValues?.fragrance} placeholder="Select fragrance">
-                                        {
-                                            flowerFragrances?.map((fragrance) => <Select.Option value={fragrance}>{fragrance.toUpperCase()}</Select.Option>)
-                                        }
-                                    </Select>
-                                </Form.Item>
-                                <Form.Item style={{ textAlign: "center" }}>
-                                    <Button size='large' type="primary" htmlType="submit">
-                                        {!itemId && !data && "Add Flower"}{type === "update" && "Update Flower"}{type === "variant" && "Make Variant"}
-                                    </Button>
-                                </Form.Item>
-                            </Form>
-                        </Flex>
-                    </Flex>}
+                        <Row justify='center' align='middle'>
+                            <Col span={24} style={{ width: "100%", padding: "20px" }}>
+                                <Form
+                                    layout="vertical"
+                                    initialValues={defaultValues}
+                                    onFinish={onfinish}
+
+                                >
+                                    <Flex gap={10} justify='space-between'>
+                                        <Form.Item style={{ width: "100%" }} name={"name"} label="Flower Name" rules={[{ required: isRequired, message: 'Please input your flower name!' }]}>
+                                            <Input placeholder='Enter Flower Name' />
+                                        </Form.Item>
+                                        <Form.Item style={{ width: "100%" }} name={"price"} label="Price per piece" rules={[{ required: isRequired, message: 'Please input your flower price!' }]}>
+                                            <Input type='number' placeholder='Enter Price' />
+                                        </Form.Item>
+                                    </Flex>
+                                    <Form.Item style={{ width: "100%" }} label="Upload picture" rules={[{ required: isRequired, message: 'Please upload your flower photo!' }]} >
+                                        {/* <Upload>
+                                            <Input type='file' onChange={(e)=>uploadImage(e)} placeholder='Picture' />
+                                        </Upload> */}
+                                        <Upload
+                                            // listType='picture-card'
+                                            {...props}
+                                        // onChange={ (info) => uploadImage(info) }
+                                        >
+                                            {/* <Button icon={<UploadOutlined />} disabled={imageUrl !== ''}> */}
+                                            Upload
+                                            {/* </Button> */}
+                                        </Upload>
+                                    </Form.Item>
+                                    <Form.Item style={{ width: "100%" }} name={"quantity"} label="Available quantity" rules={[{ required: isRequired, message: 'Please input your flower quantity!' }]} >
+                                        <Input type='number' placeholder='Enter quantity' />
+                                    </Form.Item>
+                                    <Form.Item style={{ width: "100%" }} name={"color"} label="Color of the Flower" rules={[{ required: isRequired, message: 'Please input your flower color!' }]}>
+                                        <Input placeholder='Enter color' />
+                                    </Form.Item>
+                                    <Form.Item style={{ width: "100%" }} name={"bloomDate"} label="Approximate bloom date">
+                                        <Input type={'date' || "text"} placeholder='Enter date' />
+                                    </Form.Item>
+                                    <Form.Item style={{ width: "100%" }} name={"style"} label="Flower Style">
+                                        <Input placeholder='Style' />
+                                    </Form.Item>
+                                    <Form.Item style={{ width: "100%" }} name={"arrangement"} label="Flower Arrangement">
+                                        <Input placeholder='Arrangement' />
+                                    </Form.Item>
+                                    <Form.Item style={{ width: "100%" }} name={"type"} label="Select flower category" rules={[{ required: isRequired, message: 'Please select your flower type!' }]} >
+                                        <Select placeholder="Select Flower Type">
+                                            {
+                                                flowerCategoryType?.map((type) => <Select.Option key={type} value={type}>{type.toUpperCase()}</Select.Option>)
+                                            }
+                                        </Select>
+                                    </Form.Item>
+                                    <Form.Item style={{ width: "100%" }} name={"size"} label="Select Available Size" rules={[{ required: isRequired, message: 'Please input available flower sizes!' }]} >
+                                        <Select placeholder="Select Size">
+                                            {
+                                                flowerSizes?.map((size) => <Select.Option key={size} value={size}>{size.toUpperCase()}</Select.Option>)
+                                            }
+                                        </Select>
+                                    </Form.Item>
+                                    <Form.Item style={{ width: "100%" }} name={"fragrance"} label="Fragrance Profile" rules={[{ required: isRequired, message: 'Please input your flower fragrance!' }]}>
+                                        <Select placeholder="Select fragrance">
+                                            {
+                                                flowerFragrances?.map((fragrance) => <Select.Option key={fragrance} value={fragrance}>{fragrance.toUpperCase()}</Select.Option>)
+                                            }
+                                        </Select>
+                                    </Form.Item>
+                                    <Form.Item style={{ textAlign: "center", width: "100%" }}>
+                                        <Button size='large' type="primary" htmlType="submit">
+                                            {!itemId && !data && "Add Flower"}{type === "update" && "Update Flower"}{type === "variant" && "Make Variant"}
+                                        </Button>
+                                    </Form.Item>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </div>}
             </>
     );
 };
--- src/utils/uploadImage.tsx
+++ src/utils/uploadImage.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import toast from "react-hot-toast";
 
-export const uploadImage = async (event: any) => {
-    // console.log(event.target.files[0]);
-    if (!event.target.files[0]) return;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const uploadImage = async (file: any) => {
     const formData = new FormData();
-    formData.append("image", event.target.files[0]);
+    formData.append("image", file);
     const toastId = toast.loading("Image uploading...");
     try {
         const res = await fetch(
@@ -15,15 +13,24 @@ export const uploadImage = async (event: any) => {
                 body: formData,
             }
         );
-        if (!res.ok) throw new Error("Failed to upload image");
-
+        if (!res.ok) {
+            const errorData = await res.json();
+            console.error("ImgBB API Error:", errorData);
+            throw new Error("Failed to upload image");
+        }
         const data = await res.json();
-        // console.log(data);
+        console.log(data);
+        // setImageUrl(data?.data?.display_url);
+        const url = data?.data?.display_url;
         toast.dismiss(toastId);
         toast.success("Image uploaded successfully!");
-        return (data?.data?.display_url);
-    } catch (error) {
+        return url;
+    } catch (error: any) {
+        console.error("Image upload error:", error.message);
         toast.error("Image not uploaded!");
         toast.dismiss(toastId);
+        return;
     }
+
+
 };
