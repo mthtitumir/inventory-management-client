66e4e5d2afb3fca80727d59196f9b3b6d7709321
mthtitumir <titumirgccc@gmail.com>
2024-05-21 21:51:52 +0600

    private route modified and secured

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -2,14 +2,15 @@ import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useAppSelector } from "../redux/hooks";
 import { useCurrentToken } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
-const PrivateRoute = ({children}: {children: ReactNode}) => {
-  // const token = undefined;
+const PrivateRoute = ({ children }: { children: ReactNode }) => {
   const token = useAppSelector(useCurrentToken);
-  if(!token) {
-    return <Navigate to="/login" replace={true} />
+  const user = verifyToken(token as string);
+  if (!token || !user) {
+    return <Navigate to="/login" replace={true} />;
   }
   return children;
-}
+};
 
-export default PrivateRoute
+export default PrivateRoute;
